7de60df353505f9f2df8f66a032bf784a0f25a4f
Nam Duc <nam54828@donga.edu.vn>
2022-11-16 21:27:51 +0700

    first commit

---
+++ frontend/src/components/slider/sliderData.js
@@ -0,0 +1,17 @@
+export const sliderData = [
+    {
+      image: "https://img1.kienthucvui.vn/uploads/2021/12/23/hinh-anh-trai-cay-cac-loai_094110718.jpg",
+      heading: "Slide One",
+      desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "https://tapp.vn/20-hinh-trai-cay-cute-hinh-trai-dau-cho-dien-thoai-may-tinh/image-20-hinh-trai-cay-cute-hinh-trai-dau-cho-dien-thoai-may-tinh_1_87_700.jpg",
+      heading: "Slide Two",
+      desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "https://doanhnhanplus.vn/wp-content/uploads/2019/04/dnp-10-dieu-thu-vi-ve-trai-cay-ma-ban-chua-biet-ktnn1012-240419-3.jpg",
+      heading: "Slide Three",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+  ];
